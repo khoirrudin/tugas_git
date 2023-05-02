00a80e5b7aadcb54fada45b0a83b7499d67ec64f
Iru <129159637+khoirrudin@users.noreply.github.com>
2023-05-02 09:57:50 +0700

    Create nama.php
    file php menampilkan nama

---
+++ nama.php
@@ -0,0 +1,15 @@
+<?php
+$nama = "Khoirrudin"
+$alamat = "Slogohimo"
+?>
+<!doctype html>
+<html>
+<body style="background-color: powderblue">
+    <h1>
+        <?= "Halo namaku " . $nama ?>
+    </h1>
+    <h3>
+        <?= "Aku berasal dari " . $alamat ?>
+    </h3>
+</body>
+</html>
